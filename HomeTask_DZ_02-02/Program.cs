a3388e3cf26aec99ea0ee373eebc90bc3f104ad9
Рафаэль Мухамедьяров <Rafael17IS16@gmail.com>
2023-11-12 11:50:19 +0300

    Add project files.

---
+++ HomeTask_DZ_02-02/Program.cs
@@ -0,0 +1,10 @@
+﻿string str = "hello";
+
+while (str!= "exit")
+{
+    Console.WriteLine("Выполняется цикл.\n" +
+        "для выхода введите: \"exit\"");
+    str = Console.ReadLine()!;
+}
+
+Console.WriteLine("Цикл завершился.");
